4db3af5e10d1480e2a1bf14899246101029c4d0c
Andy Olson <andyolson09@gmail.com>
2019-03-21 19:48:59 -0500

    wrap app with Provider

--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import state from './state';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import state from './state'
+import store from './store'
+import { Provider } from 'react-redux'
 
 const {
-dateTime,
-newComments,
-newTasks,
-newOrders,
-tickets,
-orders,
-taskItem,
-tasks,
-messages
-} = state;
+  dateTime,
+  newComments,
+  newTasks,
+  newOrders,
+  tickets,
+  orders,
+  taskItem,
+  tasks,
+  messages
+} = state
 
 ReactDOM.render(
-  <App 
-    taskItem={taskItem}
-    dateTime={dateTime}
-    newComments={newComments}
-    newTasks={newTasks}
-    newOrders={newOrders}
-    tickets={tickets}
-    orders={orders} 
-    messages={messages}
-    tasks={tasks}
-    />,
-  document.getElementById("root")
-);
+  <Provider store={store}>
+    <App
+      taskItem={taskItem}
+      dateTime={dateTime}
+      newComments={newComments}
+      newTasks={newTasks}
+      newOrders={newOrders}
+      tickets={tickets}
+      orders={orders}
+      messages={messages}
+      tasks={tasks}
+    />
+  </Provider>,
+  document.getElementById('root')
+)
